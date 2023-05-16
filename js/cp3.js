feb73388241ae3fbee546164db644fb96602315f
Ousadoo <Pedro.gomesf2004@gmail.com>
2023-05-16 14:34:03 -0300

    function criada.

---
+++ js/cp3.js
@@ -0,0 +1,7 @@
+function ValidacaoUsuario(inputName, inputLastName, inputEmail, inputPass, confirmPass) {
+    console.log(inputName);
+    console.log(inputLastName);
+    console.log(inputEmail);
+    console.log(inputPass);
+    console.log(confirmPass);
+}
