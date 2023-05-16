c56ab1a86f448ef8114206a86367d2d335e6fc53
davidbryan20 <davidbryan20@icloud.com>
2023-05-16 18:53:05 -0300

    Feito atualizações no html e no js

--- js/cp3.js
+++ js/cp3.js
@@ -1,43 +1,33 @@
-function ValidacaoUsuario(inputName, inputLastName, inputEmail, inputPass, confirmPass) {
-    console.log(inputName);
-    console.log(inputLastName);
-    console.log(inputEmail);
-    console.log(inputPass);
-    console.log(confirmPass);
-
-    if(inputName.value != "" && inputLastName.value != "" && inputEmail.value != "" && inputPass.value != "" && confirmPass != "") {
-        return true;
-    }else{
-        return false;
-    }
-}
-
     const inputName = document.querySelector('#idName');
-    const inputLastName = document.querySelector('#idLastName')
-    const inputEmail = document.querySelector('#idEmail')
-    const inputPass = document.querySelector('#idPassord')
-    const confirmPass = document.querySelector('#idConfirmPassord')
+    const inputLastName = document.querySelector('#idLastName');
+    const inputEmail = document.querySelector('#idEmail');
+    const inputPassword = document.querySelector('#idPassord');
+    const inputconfirmPassword = document.querySelector('#idConfirmPassord');
 
-    const labelName = document.querySelector("label[for='idName']")
-    const labelLastName = document.querySelector("label[for='idLastName']")
-    const labelEmail = document.querySelector("label[for='idEmail']")
-    const labelPassword = document.querySelector("label[for='idPassword']")
-    const labelConfirmPassword= document.querySelector("label[for='idConfirmPassword']")
+    const labelName = document.querySelector("label[for='idName']");
+    const labelLastName = document.querySelector("label[for='idLastName']");
+    const labelEmail = document.querySelector("label[for='idEmail']");
+    const labelPassword = document.querySelector("label[for='idPassword']");
+    const labelConfirmPassword= document.querySelector("label[for='idConfirmPassword']");
 
-    const submitButton = document.getElementById("btnSubmit")
+    const submitButton = document.getElementById("btnSubmit");
 
-    const greenColor = "color:#00CC00"
-    const redColor = "color:#990000"
+    const greenColor = "color:#00CC00";
+    const redColor = "color:#990000";
 
     let isNameValidated = false;
-    let isEmailValid = 0
+    let isLastNameValidated = false;
+    let isEmailValidated = false;
+    let isEmailValid;
+    let isPasswordValidated = false;
+    let isConfirmPasswordValidated = false;
 
-    function tryValidate(nameValidation, lastNameValidatioon, emailValidation, passwordValidation, confirmPasswordValidation){
+    function tryValidate(){
         try{
             if(nameValidation === false){ 
                 throw new Error("O Nome está invalido.")
             }
-            if(lastNameValidatioon === false){
+            if(lastNameValidation === false){
                 throw new Error("O ultimo nome está Inválido.")
             }
             if(emailValidation === false){
@@ -57,43 +47,80 @@ function ValidacaoUsuario(inputName, inputLastName, inputEmail, inputPass, confi
     }
 
     function nameValidation(){
+
         if(inputName.value.length >=5){
             labelName.setAttribute("style", String(greenColor));
-            isNameValidated = true
+            isNamevalidated = true
         }
-        else {
+        else{
             labelName.setAttribute("style", String(redColor))
+            isNamevalidated = false
         }
     }
     inputName.addEventListener("keyup", () => nameValidation());
 
     function lastNameValidation(){
+
         if(inputLastName.value.length >=5){
             labelLastName.setAttribute("style", String(greenColor));
             isLastNameValidated = true
         }
-        else {
+        else{
             labelLastName.setAttribute("style", String(redColor))
+            isLastNameValidated = false
         }
     }
+
+    inputLastName.addEventListener("keyup", () => lastNameValidation());
 
     function emailValidation(){
 
         isEmailValid = inputEmail.includes("@")
     
-        if (isEmailValid === true){
-            labelEmail.setAttribute("style", String(greenColor))
+            if (isEmailValid === true){
+                labelEmail.setAttribute("style", String(greenColor))
+                isEmailValidated = true
+            }
+            else{
+                labelEmail.setAttribute("style", String(redColor))
+                isEmailValidated = false
+            }
         }
-        else{
-             labelName.setAttribute("style", String(redColor))
-        }
-    }
-    
+        
     inputEmail.addEventListener("keyup", () => emailValidation());
 
+    function passwordValidation(){
 
-    
-submitButton.addEventListener("click",() => tryValidate())
+        if(inputPassword.value.length >=5 && inputPassword.value.length <= 8){
+            labelPassword.setAttribute("style", String(greenColor));
+            isPasswordValidated = true
+        }
+        else{
+            labelPassword.setAttribute("style", String(redColor))
+            isPasswordValidated = false
+        }
+    }
+
+    inputPassword.addEventListener("keyup", () => passwordValidation());
+
+    function confirmPasswordValidation(){
+
+        if(inputConfirmPassword.value.length >=5 && inputConfirmPassword.value.length <= 8 && inputConfirmPassword.value === inputPassword.value){
+            labelConfirmPassword.setAttribute("style", String(greenColor));
+            isConfirmPasswordValidated = true
+        }
+        else{
+            labelConfirmPassword.setAttribute("style", String(redColor))
+            isConfirmPasswordValidated = false
+        }
+    }
+
+    inputConfirmPassword.addEventListener("keyup", () => confirmPasswordValidation());
+
+    submitButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        tryValidate();
+    });
 
 
 
